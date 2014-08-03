df737ca39b81be7a02992fa2465872b63b455ed2
Hung Quoc Ly <hung.ly.q@gmail.com>
2014-08-03 13:20:16 +0700

    Added player and controller
    Player added, script for moving player finished, camera follow player
    movement and rotate with player.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float cameraRotaionTime;
+	private Vector3 offset;
+	private float currentVelocity;
+
+	// Use this for initialization
+	void Start () {
+		// get the current camera position
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// match the cammera positon position with the player ship
+		transform.position = player.rigidbody.position + offset;
+		transform.rotation = Quaternion.Euler (player.rigidbody.rotation.eulerAngles.x, player.rigidbody.rotation.eulerAngles.y,
+		                                                   Mathf.SmoothDampAngle(transform.rotation.eulerAngles.z, player.rigidbody.rotation.eulerAngles.z, ref currentVelocity, cameraRotaionTime));
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float playerHorizontalSpeed;
+	public float playerTiltFactor;
+	public float levelWidth;
+	public float movementSmoothTime;
+	private float previousMovement;
+	private float currentVelocity;
+
+	// Use this for initialization
+	void Start ()
+	{
+		previousMovement = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	// FixedUpdate is called once per frame before applying physics
+	void FixedUpdate ()
+	{
+		// get the ammount of device rotation and use it to determine the movement speed
+		float horizontalMovement = Input.acceleration.x;
+
+		// apply the movement vector to the player ship
+		rigidbody.velocity = new Vector3 (Mathf.SmoothDamp (previousMovement, horizontalMovement, ref currentVelocity, movementSmoothTime), 0.0f, 0.0f) * playerHorizontalSpeed;
+
+		// make sure the player does not go outside of level bound
+		rigidbody.position = new Vector3 (Mathf.Clamp (rigidbody.position.x, -levelWidth, levelWidth), rigidbody.position.y, 0.0f);
+
+		// rotate the ship
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -playerTiltFactor);
+		
+		previousMovement = horizontalMovement;
+	}
+}
