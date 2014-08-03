d6cfd7eff82f9bd9ecea9e312379fce471a719d7
Hung Quoc Ly <hung.ly.q@gmail.com>
2014-08-03 20:34:16 +0700

    Rework code for player
    Rework code for player controller, still have issue about player been
    hit and get into uncontrollable spin/positon

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody cameraRigibody;
+	public float smoothTime;
+	private float yPositionCurrentVelocity;
+	private float zPositionCurrentVelocity;
+	private float xRotationCurrentVelocity;
+	private float yRotationCurrentVelocity;
+
+	void FixedUpdate ()
+	{
+		StablizePosition ();
+		StablizeRotation ();
+	}
+
+	void StablizePosition() {
+		rigidbody.position = new Vector3 (
+			cameraRigibody.position.x,
+			Mathf.SmoothDamp (rigidbody.position.y, 0.0f, ref yPositionCurrentVelocity, smoothTime),
+			Mathf.SmoothDamp (rigidbody.position.z, 0.0f, ref zPositionCurrentVelocity, smoothTime)
+		);
+	}
+
+	void StablizeRotation () {
+		rigidbody.rotation = Quaternion.Euler (
+			Mathf.SmoothDamp (rigidbody.rotation.eulerAngles.x, 0.0f, ref xRotationCurrentVelocity, smoothTime),
+			Mathf.SmoothDamp (rigidbody.rotation.eulerAngles.y, 0.0f, ref yRotationCurrentVelocity, smoothTime),
+			cameraRigibody.rotation.eulerAngles.z
+			);
+	}
+}
