72813ccd9b998b07872e0c61ac6b5e4fdd484c4f
Hung Quoc Ly <hung.ly.q@gmail.com>
2014-08-05 11:55:03 +0700

    Explosion for bullet

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -3,4 +3,20 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour
 {
+	public float explosionForce;
+	public float explosionRadius;
+
+	void OnCollisionEnter (Collision collision)
+	{
+		// if the bullet hit something that has a rigidbody
+			Destroy(gameObject);
+
+			Collider[] colliders = Physics.OverlapSphere (collision.contacts [0].point, explosionRadius);
+
+			foreach (Collider c in colliders) {
+				if (c.rigidbody != null) {
+					c.rigidbody.AddExplosionForce (explosionForce, collision.contacts[0].point, 3, 0, ForceMode.Impulse);
+				}
+			}
+	}
 }
