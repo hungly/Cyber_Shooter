acc87834d07d631c7554b5de745f707842d48309
Ly Quan Liem <liemlyquan@gmail.com>
2014-08-13 17:31:07 +0700

    3J and script for generating randomly added

---
+++ Assets/Scripts/Boundary/GenerateRandom.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateRandom : MonoBehaviour
+{
+	public Object[] levelSegment = Resources.LoadAll("Prefabs/Leves/5");
+	private bool alreadySpawn = false;
+	
+	void OnTriggerEnter (Collider other)
+	{
+		int randomInt = Random.Range (0, levelSegment.Length - 1);
+		GameObject spawnObject = (GameObject) levelSegment[randomInt];
+		if (other.tag == "Boundary" && !alreadySpawn && spawnObject != null) {
+			Instantiate (spawnObject, transform.position + Vector3.forward * 20, other.transform.rotation);
+			alreadySpawn = true;
+		}
+	}
+}
