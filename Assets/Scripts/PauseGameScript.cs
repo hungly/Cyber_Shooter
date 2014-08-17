8ee88bac8b29074d01e157a22044dfea6e108e64
Ly Quan Liem <liemlyquan@gmail.com>
2014-08-17 09:26:28 +0700

    Add pause game, not very responsive though

---
+++ Assets/Scripts/PauseGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGameScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < Input.touchCount; i++) {
+			if (guiText.HitTest(Input.GetTouch (i).position) && Input.GetTouch (i).phase == TouchPhase.Ended) {
+				if (Time.timeScale != 0){
+					Time.timeScale = 0;
+					break;
+				} else {
+					Time.timeScale = 1;
+					break;
+				}
+			}
+		}
+	}
+}
