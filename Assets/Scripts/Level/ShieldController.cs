a67701befd6c57f10aec1e55e4265ca593f3893c
Hung Quoc Ly <hung.ly.q@gmail.com>
2014-08-24 17:25:50 +0700

    Finished level 2, fixed level 1
    Adding prefabs for level 2
    Fixed and extended level 1
    Each level now has 40 segments and need about 1 minute to complete

--- Assets/Scripts/Level/ShieldController.cs
+++ Assets/Scripts/Level/ShieldController.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class ShieldController : MonoBehaviour
 {
-	public float smoothTime = 2f;
+	public float speed = 1f;
 	private int xTarget;
 	private Vector3 currentVelocity = Vector3.zero;
+	private float startTime;
+	private float journeyLength;
+	private Vector3 endPoint;
 
 	// Use this for initialization
 	void Start ()
@@ -16,20 +19,28 @@ public class ShieldController : MonoBehaviour
 			0.0f,
 			0.0f
 		));
+		startTime = Time.time;
+		endPoint = new Vector3 (xTarget, transform.localPosition.y, transform.localPosition.z);
+		journeyLength = Vector3.Distance (transform.localPosition, endPoint);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.position = Vector3.SmoothDamp (
-			transform.position,
-			new Vector3 (xTarget, transform.position.y, transform.position.z),
-			ref currentVelocity,
-			smoothTime
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+
+		transform.localPosition = Vector3.Lerp (
+			transform.localPosition,
+			endPoint,
+			fracJourney
 		);
 
-		if (Mathf.Abs (transform.position.x - xTarget) < 0.5f) {
+		if (Mathf.Abs (transform.localPosition.x - xTarget) < 0.2f) {
 			xTarget *= -1;
+			
+			startTime = Time.time;
+			endPoint = new Vector3 (xTarget, transform.localPosition.y, transform.localPosition.z);
 		}
 	}
 }
